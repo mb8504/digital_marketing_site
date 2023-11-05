f2beae65faf283499b85dae3bc0eb63fef4e218c
mb8504 <mbergmarketing@gmail.com>
2023-11-05 05:59:33 -0500

    updated mobile nav

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,7 @@ const navToggle = document.getElementById('mobile-navbar');
 
 toggleButton.addEventListener('click', () => {
     navToggle.classList.toggle('active'); 
+    toggleButton.classList.toggle('active');
     const visibility = toggleButton.getAttribute('aria-expanded');
     if (visibility === 'false') {
         toggleButton.setAttribute("aria-expanded", true)
